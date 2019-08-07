62600a6105052fa2731229fd3f3152ad0c52c9dd
Oliwood89 <oliloriaux@gmail.com>
2019-08-07 11:34:37 +0200

    Updates

---
+++ precache-manifest.abf20543c330dc58943294438657952b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed440e2db4d8966e0f30196c31cceb8",
+    "url": "/happydrink/index.html"
+  },
+  {
+    "revision": "c91c749c701bd51913d2",
+    "url": "/happydrink/static/css/main.ebce302d.chunk.css"
+  },
+  {
+    "revision": "1f1e70a61d94405a999b",
+    "url": "/happydrink/static/js/2.158f2d9c.chunk.js"
+  },
+  {
+    "revision": "c91c749c701bd51913d2",
+    "url": "/happydrink/static/js/main.c6764d3d.chunk.js"
+  },
+  {
+    "revision": "b7e30ab0261c2526cb0e",
+    "url": "/happydrink/static/js/runtime~main.67011007.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/happydrink/static/media/logo.5d5d9eef.svg"
+  }
+]);
